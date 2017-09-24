82606216c5bb01a91774b9ab86ed2e4bd9474429
sunhuafeng1995 <32211182+sunhuafeng1995@users.noreply.github.com>
2017-09-24 17:36:53 +0800

    计算器

--- proTest/src/pro0924/CumputTest.java
+++ proTest/src/pro0924/CumputTest.java
@@ -1,25 +1,44 @@
 package pro0924;
 
+import java.util.Scanner;
+
 public class CumputTest {
+
+	private static Scanner sc;
 
 	/*
 	 * add 求和
+	 * 法则 + - * /
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 1;
-		int b = 2;
 		
-		int result;
-		result = add(a, b);
+		int a;
+		int b;
+		int result = 0;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("欢迎:");
+		
+		System.out.println("1geshu ");
+		a = sc.nextInt();
+		
+		System.out.println("2geshu ");
+		b = sc.nextInt();
+		
+		System.out.println("什么法则?");
+		String str = sc.next();
+		
+		if(str.equals("+")) {
+			result = add(a, b);
+		}
+		
 		
 		System.out.println("结果是:" + result);
 	}
 
 	private static int add(int a, int b) {
-		// TODO Auto-generated method stub
-		System.out.println("sss");
-		return 2222;
+		return a+b;
 	}
 
 }
