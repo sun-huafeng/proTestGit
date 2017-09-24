75b842eff6af100c164aa0df8aa06936f7221dc2
sunhuafeng1995 <32211182+sunhuafeng1995@users.noreply.github.com>
2017-09-24 17:23:29 +0800

    求和

---
+++ proTest/src/pro0924/CumputTest.java
@@ -0,0 +1,25 @@
+package pro0924;
+
+public class CumputTest {
+
+	/*
+	 * add 求和
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		
+		int result;
+		result = add(a, b);
+		
+		System.out.println("结果是:" + result);
+	}
+
+	private static int add(int a, int b) {
+		// TODO Auto-generated method stub
+		System.out.println("sss");
+		return 2222;
+	}
+
+}
